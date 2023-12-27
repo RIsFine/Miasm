ea07401ca6b254d27584c6b82c13299145a386a7
RIsFine <adrien.najjar@outlook.com>
2023-12-27 10:58:00 +0100

    Renamed project and recipes_final.csv created

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from GFaim.utils import *
-from GFaim.recipe import Recipe
+from Miasm.utils import *
+from Miasm.recipe import Recipe
 
 
 def get_final_recipes():
     recipes = reformat_cols(pd.read_csv("recipes.csv", sep=";"))
     list_meal = []
     list_not_meal = []
+
+    # FIRST SORT
     for i in range(recipes.shape[0]):
         meal = False
         recipe = Recipe(recipes.iloc[i])
@@ -27,6 +29,7 @@ def get_final_recipes():
             list_not_meal.append(recipe)
             # print(f"{recipe.name}: not meal, {recipe.time_cook}; {recipe.ingredients}")
 
+    # SECOND SORT
     final_meals = ([meal for meal in list_meal if not any([name in meal.name.lower() for name in not_meal_names])] +
                    [meal for meal in list_not_meal if any([name in meal.name.lower() for name in meal_names])])
 
@@ -34,15 +37,15 @@ def get_final_recipes():
 
     print(f"meal : {len(list_meal)}")
     print(f"not meal : {len(list_not_meal)}")
-
-    # for meal in final_meals:
-    #     print(f"{meal.name}, {meal.ingredients}")
     print(f"len final : {len(final_meals)}")
+
     indexes = [meal.index for meal in final_meals]
     recipes_final = recipes.iloc[indexes]
-    recipes_final.to_csv("recipes_final.csv", header=True, sep=";")
+    recipes_final.to_csv("recipes_final.csv", header=True, sep=";", columns=recipes.columns)
 
 
 if __name__ == "__main__":
-    recipes = pd.read_csv("recipes.csv", sep=";")
-    print(recipes["ingredients"])
+    recipes = reformat_cols(pd.read_csv("recipes_final.csv", sep=";"))
+    for i in range(recipes.shape[0]):
+        recipe = recipes.iloc[i]
+        print(f'{recipe["name"]}, {recipe["ingredients"]}')
